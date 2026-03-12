5c008e5983d8db024d80f326179f307259f4fa41
Eshaan Mohammed <eshaan.mohammed16@gmail.com>
2026-03-12 16:08:36 +0000

    120326

---
+++ Modules/Modules/main.py
@@ -0,0 +1,16 @@
+# import sys#built in system module 
+
+# for path in sys.path:
+#   print(path)
+# #fetches system path
+import eshaan_math
+#add_result=eshaan_math.add(2,3)
+sum = eshaan_math.add(3, 4)
+print(sum)
+sub=eshaan_math.subtract(3,4)
+print(sub)
+div=eshaan_math.divide(2,3)
+print(div)
+
+mult=eshaan_math.multiply(3,4)
+print(mult)
